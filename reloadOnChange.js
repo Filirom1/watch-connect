7b953528488c90a2414d26e8eb73cdca7b9ea84c
Romain <filirom1@gmail.com>
2012-07-12 09:19:00 +0200

    removed features not used

--- reloadOnChange.js
+++ reloadOnChange.js
@@ -14,29 +14,19 @@ var fs = require('fs'),
   socketio = require('socket.io'),
   logio = new (require('socket.io/lib/logger'))(),
   connect = require('connect'),
-  mustache = require('mustache'),
   _ = require('underscore'),
-  mime = connect.static.mime,
   watchTree = require("fs-watch-tree").watchTree,
   sockets = {},
   // one time-hit, get the file content of the socket.io client side script
-  ioScript = (function() {
-    var template = fs.readFileSync(path.join(__dirname, './util/socket-enable.js'), 'utf8');
-    // emulate a precompiled mustache template, mainly trying to keep
-    // the same usage as a precompiled underscore or handlebar template.
-    return function() {
-      return mustache.to_html(template, arguments[0], arguments[1]);
-    };
-  })();
-
+  ioScript = fs.readFileSync(path.join(__dirname, './util/socket-enable.js'), 'utf8');
 
 module.exports = function(dirToWatch, server, options){
   options = options || {};
-  if(!fs.statSync(dirToWatch)) return console.error('Unable to watch ' + dirToWatch, err.message);
+  if(!fs.statSync(dirToWatch)) return console.error('[watch-connect]', 'Unable to watch ' + dirToWatch, err.message);
 
   watchTree(dirToWatch, { exclude: [".git", "node_modules", ".hg"] }, function (event) {
     if (options.verbose) {
-      console.log("File named: " + event.name + "has changed");
+      console.log('[watch-connect]', "File named: " + event.name + " has changed");
     }
     emit(options);
   });
@@ -48,20 +38,14 @@ module.exports = function(dirToWatch, server, options){
   io.enable('browser client gzip');
   io.set('log level', 1);
 
-  // store the socket object in grunt's config, so that we can interract from other tasks
   io.sockets.on('connection', function(socket) {
-    // should support multiple-connections
-    // raw first attempt
 
-    // should also catch up client disconnection and clean
-    // the stored socket
     socket.on('disconnect', function() {
-      if(options.verbose) console.log('[socket.io] Remove client ' + socket.id);
+      if(options.verbose) console.log('[watch-connect]', 'Remove client ' + socket.id);
       if(sockets[socket.id]) delete sockets[socket.id];
-      // restore sockets back in config
     });
 
-    if(options.verbose) console.log('[socket.io] Add client ' + socket.id);
+    if(options.verbose) console.log('[watch-connect]', 'Add client ' + socket.id);
     sockets[socket.id] = socket;
   });
 
@@ -73,29 +57,31 @@ module.exports = function(dirToWatch, server, options){
       filepath = path.normalize(path.join(dirToWatch, decodeURIComponent(parsed.pathname))),
 
       // index.html support when trainling `/`
-      index = path.normalize('/') === filepath.charAt(filepath.length - 1),
+      index = path.normalize('/') === filepath.charAt(filepath.length - 1);
 
-      ioResponse;
 
     if(index) filepath += 'index.html';
 
     // deal with our special socket.io client-side script
     if(path.basename(filepath) === 'socket-enable.js') {
-      res.setHeader('Content-Type', mime.lookup('js'));
-      ioResponse = ioScript({});
-      res.setHeader('Content-Length', ioResponse.length);
-      return res.end(ioResponse);
+      res.setHeader('Content-Type', 'application/javascript');
+      res.setHeader('Content-Length', ioScript.length);
+      return res.end(ioScript);
     }
 
     // skip adding assumes socket.js scripts have been hand
     // added to resulting template
-    if (options.skipAdding) {
-      return next();
-    }
+    if (options.skipAdding) return next(); 
+
+    // skip non html files
+    if (path.extname(filepath) != '.html') return next();
 
     fs.stat(filepath, function(e, stat) {
       // files do not exists, next with error only on unexpected errors
-      if(e) return next((e.code === 'ENOENT' || 'ENAMETOOLONG' === e.code) ? null : e);
+      if(e) {
+        if(options.verbose) console.error('[watch-connect]', e);
+        return next();
+      }
 
       // file is a dir, next to the directory listing if enabled
       if(stat.isDirectory()) return next();
@@ -105,22 +91,22 @@ module.exports = function(dirToWatch, server, options){
 
       // setup some basic headers, might add some. Below is pretty minimalist (might tweak and add
       // basic caching stuff for example)
-      res.setHeader('Content-Type', mime.lookup(filepath));
+      res.setHeader('Content-Type', 'text/html');
 
       // can't use the ideal stream / pipe case, we need to alter the html response
       // by injecting that little socket.io client-side app.
       fs.readFile(filepath, 'utf8', function(e, body) {
         if(e) return next(e);
 
+        if(options.verbose) console.log('[watch-connect]', 'Append scripts in ' + filepath);
         body = body.replace(/<\/body>/, function(w) {
           return [
             '  <script defer src="/socket.io/socket.io.js"></script>',
-            '  <script defer src="/socket-enable.js"></script>',
-            w
+            '  <script defer src="/socket-enable.js"></script>'
           ].join('\n');
         });
 
-        //res.setHeader('Content-Length', body.length);
+        res.setHeader('Content-Length', body.length);
         res.end(body);
       });
     });
@@ -129,12 +115,10 @@ module.exports = function(dirToWatch, server, options){
 
 // basic reload tasks, works in tandem with serve task
 function emit(options){
-  if(options.verbose) console.log('Changes detected');
-  var errors = [];
-  if(options.verbose && _.isEmpty(sockets)) return console.error('No client connected to socket.io');
+  if(options.verbose) console.log('[watch-connect]', 'Changes detected');
+  if(options.verbose && _.isEmpty(sockets)) return console.error('[watch-connect]', 'No client connected to socket.io');
   Object.keys(sockets).forEach(function(s) {
-    if(errors.length) return sockets[s].emit('error', errors);
-    if(options.verbose) console.log('[socket.io] reload clients');
+    if(options.verbose) console.log('[watch-connect]', 'reload clients');
     sockets[s].emit('changed');
   });
 }
--- util/socket-enable.js
+++ util/socket-enable.js
@@ -2,50 +2,14 @@
 
   // todo, pass the hostname too, will be required
   // for the socket connection to work remotely (eg. not only on local machine)
-  var url = 'http://{{hostname}}:{{ port }}';
+  var url = 'http://localhost';
     socket = io.connect(url);
 
   // watched files just changed, reload page.
   // also retrigger on reconnect event
   socket
     .on('changed', reload)
-    .on('reconnect', reload)
-    .on('running', function() {
-      console.log('running man', arguments);
-      notify([{ msg: 'Running man <span class="waiting"></span>' }]);
+    .on('reconnect', reload);
 
-      var wait = $('.waiting');
-      document.title = 'Running man...';
-      (function waiting(){
-        setTimeout(function() {
-          wait.append('.');
-          document.title = document.title + '.';
-          waiting();
-        }, 500);
-      })();
-    })
-    .on('error', notify);
-
-  function notify(errors) {
-    var div = document.createElement('div'),
-      errDiv = document.getElementById('mockerie-error'),
-      first = document.body.querySelector('*');
-
-    div.innerHTML = errors.map(function(err) {
-      return err.msg;
-    }).join('<hr \/>');
-
-    div.id = 'mockerie-error';
-    div.style.color = '#b94a48';
-    div.style.padding = '1em';
-    div.style.backgroundColor = '#f2dede';
-    div.style.borderColor = '#eed3d7';
-    div.style.position = 'fixed';
-    div.style.width = '100%';
-
-    if(errDiv) errDiv.innerHTML = div.innerHTML;
-    else document.body.insertBefore(div, first);
-  }
   function reload() { location.reload(); }
-
 })();
