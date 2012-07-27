4a0f1244b16d51f1379338e2f8a0bd247d2bc38b
dbashford <dbashford@bericotechnologies.com>
2012-07-27 17:00:19 -0400

    removing test code

--- reloadOnChange.js
+++ reloadOnChange.js
@@ -29,7 +29,7 @@ module.exports = function(options){
   if(!options || !options.watchdir || !options.server) return console.error('[watch-connect]', 'Options missing');
   if(!fs.statSync(options.watchdir)) return console.error('[watch-connect]', 'Unable to watch ' + options.watchdir, err.message);
 
-  var exclude = [".git", "node_modules", ".hg", "zoom-panel.png"];
+  var exclude = [".git", "node_modules", ".hg"];
   if (options.exclude) {
     exclude = exclude.concat(options.exclude);
   }
