8eaac0d2f7c1fdf87ca72b5b617e747e776d1036
dbashford <dbashford@bericotechnologies.com>
2012-08-13 09:43:42 -0400

    references #6

--- reloadOnChange.js
+++ reloadOnChange.js
@@ -19,22 +19,31 @@ var fs = require('fs'),
   ioScript = fs.readFileSync(path.join(__dirname, './util/socket-enable.js'), 'utf8');
 
 // Possible options are :
-// * watchdir: path to the directory to watch (required)
+// * watchdir: string path the directories to watch (required)
 // * server: http server to hook our reload (required)
 // * basedir: path to the base directory of our served files (optional)
+// * additionaldirs: array of additional paths to watch
 // * verbose: be verbose or not (default=false)
 // * skipAdding: don't add socket io scripts, already been hand added/used
 // * exclude: files to exclude from watch, so therefore they will not force refresh of page
 module.exports = function(options){
   if(!options || !options.watchdir || !options.server) return console.error('[watch-connect]', 'Options missing');
-  if(!fs.statSync(options.watchdir)) return console.error('[watch-connect]', 'Unable to watch ' + options.watchdir, err.message);
+
+  var dirsToWatch = [options.watchdir];
+  if (options.additionaldirs) {
+    dirsToWatch = dirsToWatch.concat(options.additionaldirs);
+  }
+
+  dirsToWatch.forEach(function (dir) {
+    if(!fs.statSync(dir)) return console.error('[watch-connect]', 'Unable to watch ' + dir, err.message);
+  });
 
   var exclude = [".git", "node_modules", ".hg"];
   if (options.exclude) {
     exclude = exclude.concat(options.exclude);
   }
 
-  var watcher = watchTree.watch(options.watchdir, {persistent: true});
+  var watcher = watchTree.watch(dirsToWatch, {persistent: true});
   watcher.on('all', function(type, itemPath) {
     ignore = exclude.some(function(ele){
       return itemPath.indexOf(ele) >= 0
